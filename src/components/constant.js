a458b9cb2f2e403402b0c09329425a32425e86fd
subhrajit1990 <subhrajitsahu1990@gmail.com>
2018-07-06 07:32:09 +0530

    new read me

--- src/components/constant.js
+++ src/components/constant.js
@@ -11,15 +11,15 @@ import ProductsList from './ProductsList';
 import ProductPage from './ProductPage';
 export const routes = (
 	<Switch>
-  		<Route exact path="/" component={Food}/>
-    	<Route path="/Contact" component={Contact}/>
-    	<Route path="/About" component={About}/>
-    	<Route path="/Home" component={Home}/>
-    	<Route name="ProductsList" path="/ProductsList/:productListPath" component={ProductsList} />
-      <Route name="ProductPage" path="/ProductPage" component={ProductPage} />
-    	//<Route name="ProductsList" path="/ProductsList/:productListPath" render={(props) => (<ProductsList {...props}/>)} />
-    	
-       	<Route component={NotFoundPage}/>
+    <Route exact path="/" component={Food}/>
+  	<Route path="/Contact" component={Contact}/>
+  	<Route path="/About" component={About}/>
+  	<Route path="/Home" component={Home}/>
+  	<Route name="ProductsList" path="/ProductsList/:productListPath" component={ProductsList} />
+    <Route name="ProductPage" path="/ProductPage" component={ProductPage} />
+  	//<Route name="ProductsList" path="/ProductsList/:productListPath" render={(props) => (<ProductsList {...props}/>)} />
+  	
+    <Route component={NotFoundPage}/>
     </Switch>
 );
 

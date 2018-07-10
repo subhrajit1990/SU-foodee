efb67c430f4b735d7a5c2e23f6159656f49b9e71
subhrajit1990 <subhrajitsahu1990@gmail.com>
2018-07-10 23:43:59 +0530

    codes for blog section:Subhrajit, ticket:#

--- src/components/Blog/BlogList/index.js
+++ src/components/Blog/BlogList/index.js
@@ -2,14 +2,35 @@
 
 import React from 'react';
 import Blog from './blog';
+import requestService from '../../../api/api';
 
 const API_URL = "";
 export default class BlogList extends React.Component{
-
+	
 	constructor(props){
 		super(props);
 
+		this.state = {
+			blogs: []
+		};
+
 	}
+
+	componentDidMount(){
+		this.fetchBlogs()
+	}
+
+
+	fetchBlogs(){
+		requestService(API_URL)
+		.then(response => response.json())
+		.then(res => this.setState({
+			blogs: res.blogs
+		}));
+	}
+	
+	
+
 	render(){
 		return(
 			<div>Lorem Ipsum</div>
